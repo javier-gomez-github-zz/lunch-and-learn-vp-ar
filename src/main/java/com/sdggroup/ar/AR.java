edf7e252975fa191b9e7d42f7190f85d04993011
Javier Gómez <jgomez@macbook-air.home>
2019-05-06 22:13:31 +0200

    SDG - Standalone

---
+++ src/main/java/com/sdggroup/ar/AR.java
@@ -0,0 +1,14 @@
+package com.sdggroup.ar;
+
+public class AR {
+
+    public static void main(String[] args) {
+        if (args[0].equals("Cubes")) {
+            Cubes.main(new String[]{});
+        }
+
+        if (args[0].equals("Coordinates")) {
+            Coordinates.main(new String[]{});
+        }
+    }
+}
